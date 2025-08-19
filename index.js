6dadd22b41801613b00c4df8ca168cbfeb5d472e
Derek Griggs <webbuilt09@gmail.com>
2025-08-19 05:04:39 -0500

    rename app.js to index.js for consistency with package.json

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+res.send('Hello World');
+})
+
+app.post('/',(req,res) =>{
+    res.send('This is a post request!!')
+})
+
+app.listen(PORT,()=>{
+    console.log('App is listening on port ' + PORT)
+});
